092927a70cfac3a6918768cd548344bdfc27674a
Eder Oliveira <ederoliveira@id.uff.br>
2024-07-05 19:47:55 -0300

    Atualização no comando de leitura do sensor de pressão

--- sensor_de_pressao/bash/plot_serial.sh
+++ sensor_de_pressao/bash/plot_serial.sh
@@ -8,5 +8,8 @@ BAUD_RATE=9600
 stty -F $SERIAL_DEVICE $BAUD_RATE
 
 # Read from the serial device and plot using ttyplot
-cat $SERIAL_DEVICE | ttyplot -t "Sensor de pressão HSCDDRN060MD2A5 (Honeywell)" -u "mbar"
+cat $SERIAL_DEVICE | ttyplot -t "Leitura do sensor de pressão" -u "mbar"
+
+# Versão que que não apaga o gráfico da tela quando o termina
+#cat $SERIAL_DEVICE | TERM=vt100 ttyplot -t "Leitura do sensor de pressão" -u "mbar"
 
